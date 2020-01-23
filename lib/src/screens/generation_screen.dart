86a247c53acb65e3da8cc5821ede4968b27cb4f8
Edo <edo.g@witchery.io>
2020-01-23 19:37:50 +0400

    initial 13

--- lib/src/screens/generation_screen.dart
+++ lib/src/screens/generation_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
 class GenerationScreen extends StatelessWidget {
+  final String generateMnemonic =
+      'limit boost flip evil regret shy alert always shine cabin unique angry';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(child: Text('Generation Screen')),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Geneation Mnemonic',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              Wrap(
+                spacing: 4.0,
+                children: generateMnemonic.split(' ').map((s) {
+                  return Chip(
+                    label: Text('$s'),
+                    backgroundColor: Colors.indigoAccent,
+                  );
+                }).toList(),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
