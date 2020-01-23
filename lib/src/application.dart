be4b5eeeee37c9d7ced7c1b408102e379f426c38
Edo <edo.g@witchery.io>
2020-01-23 11:04:47 +0400

    initial 3

--- lib/src/application.dart
+++ lib/src/application.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental/core/theme.dart';
+
+import 'localization.dart';
 
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: FlutterBlocLocalizations().appTitle,
+      theme: WalletTheme.theme,
+      localizationsDelegates: [
+        FlutterBlocLocalizationsDelegate(),
+      ],
+      home: Scaffold(
+        appBar: AppBar(title: Text('Wallet'),),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[Text('Application')],
+          ),
+        ),
+      ),
+    );
   }
 }
