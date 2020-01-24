190aa93938cee64cdbb1595ec8f58bc5668ff726
Edo <edo.g@witchery.io>
2020-01-24 11:14:49 +0400

    chip integration

--- lib/src/screens/generation_screen.dart
+++ lib/src/screens/generation_screen.dart
@@ -6,28 +6,34 @@ class GenerationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> keyWords = generateMnemonic.split(' ');
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Geneation Mnemonic',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              Wrap(
-                spacing: 4.0,
-                children: generateMnemonic.split(' ').map((s) {
-                  return Chip(
-                    label: Text('$s'),
-                    backgroundColor: Colors.indigoAccent,
-                  );
-                }).toList(),
-              )
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Geneation Mnemonic',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                Wrap(
+                    spacing: 6.0,
+                    children: keyWords.asMap().entries.map((entry) {
+                      return Chip(
+                        avatar: CircleAvatar(
+                          backgroundColor: Colors.grey.shade800,
+                          child: Text('${entry.key}'),
+                        ),
+                        label: Text('${entry.value}'),
+                      );
+                    }).toList()),
+              ],
+            ),
           ),
         ),
       ),
