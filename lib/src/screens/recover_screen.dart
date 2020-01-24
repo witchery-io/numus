91a039bf9efa189b0581ddceb28040266b6fd1ee
Edo <edo.g@witchery.io>
2020-01-24 15:53:01 +0400

    chip integration - recover screen - 8

--- lib/src/screens/recover_screen.dart
+++ lib/src/screens/recover_screen.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class RecoverScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(child: Text('Recover')),
+      ),
+    );
   }
 }
