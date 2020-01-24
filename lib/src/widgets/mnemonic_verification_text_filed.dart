041ccde55f7808148bdc090c7529da0ffbe7e99c
Edo <edo.g@witchery.io>
2020-01-24 17:51:20 +0400

    Mnemonic Text filed

--- lib/src/widgets/mnemonic_verification_text_filed.dart
+++ lib/src/widgets/mnemonic_verification_text_filed.dart
@@ -3,28 +3,31 @@ import 'package:flutter/material.dart';
 class MnemonicVerificationTextField extends StatelessWidget {
   final Function onChanged;
 
+  final TextEditingController wordsController = TextEditingController();
+
   MnemonicVerificationTextField({@required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          autofocus: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: const BorderSide(color: Colors.red, width: 0.0),
-            ),
-            hintText: 'Type the words here',
-            helperText:
-                '* Please type 1, 5, 9 words in your note for verification.',
-            helperStyle: TextStyle(color: Colors.red),
-            labelText: 'Mnemonic verification words',
-            prefixIcon: Icon(Icons.vpn_key, color: Colors.red),
-          ),
-          onChanged: (words) => onChanged(words),
+    return TextField(
+      autofocus: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.red, width: 0.0),
         ),
-      ],
+        hintText: 'Type the words here',
+        helperText:
+        '* Please type 1, 5, 9 words in your note for verification.',
+        helperStyle: TextStyle(color: Colors.red),
+        labelText: 'Mnemonic verification words',
+        prefixIcon: Icon(Icons.vpn_key, color: Colors.red),
+      ),
+      onChanged: _onChange,
+      controller: wordsController,
     );
+  }
+  
+  _onChange(String words) {
+    onChanged(wordsController.text.trim().split(' '));
   }
 }
