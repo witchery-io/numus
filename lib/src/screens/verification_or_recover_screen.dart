eef81f36afca8aa2eecf3e77eae272f22c1eba6d
Edo <edo.g@witchery.io>
2020-01-25 13:29:06 +0400

    verification or recover screen 3

--- lib/src/screens/verification_or_recover_screen.dart
+++ lib/src/screens/verification_or_recover_screen.dart
@@ -27,17 +27,21 @@ class _VerificationOrRecoverScreenState
                     style: TextStyle(fontSize: 24.0)),
                 SizedBox(height: 12.0),
                 // General logic
-                mnemonic == null ? MnemonicVerificationTextField(
-                  helperText:
-                  '* Please type mnemonic (12 words) in your note for verification.',
-                  labelText: 'Recover words',
-                  onChanged: (String typedWords) => _onRecover(mnemonic, typedWords),
-                ) : MnemonicVerificationTextField(
-                  helperText:
-                  '* Please type 1, 5, 9 words in your note for verification.',
-                  labelText: 'Verification words',
-                  onChanged: (String typedWords) => _onVerified(mnemonic, typedWords),
-                ),
+                mnemonic == null
+                    ? MnemonicVerificationTextField(
+                        helperText:
+                            '* Please type mnemonic (12 words) in your note for verification.',
+                        labelText: 'Recover words',
+                        onChanged: (String typedWords) =>
+                            _onTyped(mnemonic, typedWords),
+                      )
+                    : MnemonicVerificationTextField(
+                        helperText:
+                            '* Please type 1, 5, 9 words in your note for verification.',
+                        labelText: 'Verification words',
+                        onChanged: (String typedWords) =>
+                            _onTyped(mnemonic, typedWords),
+                      ),
                 SizedBox(height: 12.0),
                 Wrap(
                     spacing: 8.0,
@@ -47,7 +51,7 @@ class _VerificationOrRecoverScreenState
                           : CustomChip(
                               index: (_verificationKeys[entry.key] + 1),
                               title: entry.value);
-                    }).toList())
+                    }).toList()),
               ],
             ),
           ),
@@ -56,13 +60,23 @@ class _VerificationOrRecoverScreenState
     );
   }
 
-  _onVerified(String mnemonic, String typedWords) {
+  _onTyped(String mnemonic, String typedWords) {
     print(mnemonic);
     print(typedWords);
+
+//    _onVerified(mnemonic, typedWords);
+//    _onRecover(mnemonic, typedWords);
+  }
+
+  _onVerified(String mnemonic, String typedWords) {
+//    print(mnemonic);
+//    print(typedWords);
+//    print(typedWords.length);
   }
 
   _onRecover(String mnemonic, String typedWords) {
-    print(mnemonic);
-    print(typedWords);
+//    print(mnemonic);
+//    print(typedWords);
+//    print(typedWords.length);
   }
 }
