50b573b508f0c6309e5c8f68d1b79509ef67ddc4
Edo <edo.g@witchery.io>
2020-01-27 16:54:54 +0400

    verification logic 3

--- lib/src/screens/verification_or_recover_screen.dart
+++ lib/src/screens/verification_or_recover_screen.dart
@@ -11,111 +11,102 @@ class _VerificationOrRecoverScreenState
     extends State<VerificationOrRecoverScreen> {
   List<String> _listWords;
   bool _isShowTags = false;
-  bool _isEnableVerifiedBtn = false;
   List<int> _verificationKeys = const [0, 4, 8];
+  Function _onApply;
+  bool _isEnableApplyBtn = false;
 
   @override
   Widget build(BuildContext context) {
     final String mnemonic = ModalRoute.of(context).settings.arguments;
     final bool isRecover = mnemonic == null;
 
-    // todo :: assets
+    assert((isRecover && mnemonic == null) || (!isRecover && mnemonic != null));
 
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           padding: EdgeInsets.all(24.0),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text(isRecover ? 'Recover' : 'Verification',
-                        style: TextStyle(fontSize: 24.0)),
-                    Text('* For split up words use space',
-                        style: TextStyle(color: Colors.deepOrange))
-                  ],
-                ),
-                SizedBox(height: 12.0),
-                isRecover
-                    ? MnemonicVerificationTextField(
-                        helperText:
-                            '* Please type mnemonic (12 words) in your note for recover.',
-                        labelText: 'Recover words',
-                        onChanged: (String typedWords) {})
-                    : MnemonicVerificationTextField(
-                        helperText:
-                            '* Please type 1, 5, 9 words in your note for verification.',
-                        labelText: 'Words verification',
-                        onChanged: (String typedWords) {
-                          /// Verification Logic
-                          final listTrimTypeWords =
-                              typedWords.trim().split(' ');
-                          final listTypeWords = typedWords.split(' ');
+          child: Column(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(isRecover ? 'Recover' : 'Verification',
+                      style: TextStyle(fontSize: 24.0)),
+                  Text('* For split up words use space',
+                      style: TextStyle(color: Colors.deepOrange))
+                ],
+              ),
+              SizedBox(height: 12.0),
 
-                          setState(() {
-                            _isShowTags = typedWords.length != 0;
+              /// Input
+              isRecover
+                  ? MnemonicVerificationTextField(
+                      helperText:
+                          '* Please type mnemonic (12 words) in your note for recover.',
+                      labelText: 'Recover words',
+                      onChanged: (String typedWords) {})
+                  : MnemonicVerificationTextField(
+                      helperText:
+                          '* Please type 1, 5, 9 words in your note for verification.',
+                      labelText: 'Words verification',
+                      onChanged: (String typedWords) {
+                        /// Verification Logic
+                        final listTrimTypeWords = typedWords.trim().split(' ');
+                        final listTypeWords = typedWords.split(' ');
 
-                            /// check available and update in list
-                            _listWords =
-                                listTypeWords.length > _verificationKeys.length
-                                    ? _listWords
-                                    : listTypeWords;
+                        setState(() {
+                          _isShowTags = typedWords.length != 0;
 
-                            /// enable confirmed btn when words count will be 3
-                            _isEnableVerifiedBtn = listTrimTypeWords.length ==
-                                    _verificationKeys.length &&
-                                listTypeWords.length ==
-                                    _verificationKeys.length;
-                          });
-                        }),
-                SizedBox(height: 12.0),
+                          /// check available and update in list
+                          _listWords =
+                              listTypeWords.length > _verificationKeys.length
+                                  ? _listWords
+                                  : listTypeWords;
 
-                /// Tags logic
-                Wrap(
-                    spacing: 8.0,
-                    children: _isShowTags
-                        ? _listWords.asMap().entries.map((entry) {
-                            /// todo ::
-                            return mnemonic == null
-                                ? CustomChip(
-                                    index: (entry.key + 1), title: entry.value)
+                          /// enable confirmed btn when words count will be 3
+                          _isEnableApplyBtn = listTrimTypeWords.length ==
+                                  _verificationKeys.length &&
+                              listTypeWords.length == _verificationKeys.length;
+                        });
 
-                                /// case insurance verification key list undefined error
-                                : entry.key < _verificationKeys.length
-                                    ? CustomChip(
-                                        index:
-                                            (_verificationKeys[entry.key] + 1),
-                                        title: entry.value)
-                                    : SizedBox.shrink();
-                          }).toList()
-                        : []),
+                        _onApply = () {
+                          print('Apply an Verified');
 
-                /// Apply btn
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      CustomButton(
-                        // todo ::
-                        child: Text('Apply'),
-                        onPressed: _isEnableVerifiedBtn
-                            ? isRecover ? _onRecovered : _onVerified
-                            : null,
-                      ),
-                    ])
-              ],
-            ),
+                          /// show Error
+                          /// OR
+                          /// Navigate to Wallet page
+                        };
+                      }),
+              SizedBox(height: 12.0),
+
+              /// Tags logic
+              Wrap(
+                  spacing: 8.0,
+                  children: _isShowTags
+                      ? _listWords.asMap().entries.map((entry) {
+                          return isRecover
+                              ? CustomChip(
+                                  index: (entry.key + 1), title: entry.value)
+
+                              /// case insurance verification key list undefined error
+                              : entry.key < _verificationKeys.length
+                                  ? CustomChip(
+                                      index: (_verificationKeys[entry.key] + 1),
+                                      title: entry.value)
+                                  : SizedBox.shrink();
+                        }).toList()
+                      : []),
+
+              /// Apply btn
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+                CustomButton(
+                    child: Text('Apply'),
+                    onPressed: _isEnableApplyBtn ? _onApply : null),
+              ])
+            ],
           ),
         ),
       ),
     );
-  }
-
-  _onRecovered() {
-    print('Recover');
-  }
-
-  _onVerified() {
-    print('Verified');
   }
 }
