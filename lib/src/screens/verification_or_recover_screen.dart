afca13bac797e82bae9906a3fe3372a210e640e9
Edo <edo.g@witchery.io>
2020-01-27 15:10:13 +0400

    verification or recover screen logic 4

--- lib/src/screens/verification_or_recover_screen.dart
+++ lib/src/screens/verification_or_recover_screen.dart
@@ -10,7 +10,7 @@ class VerificationOrRecoverScreen extends StatefulWidget {
 class _VerificationOrRecoverScreenState
     extends State<VerificationOrRecoverScreen> {
   List<String> _listWords;
-  bool _wordsIsEmpty = true;
+  bool _isShowWords = false;
   List<int> _verificationKeys = const [0, 4, 8];
 
   @override
@@ -34,11 +34,10 @@ class _VerificationOrRecoverScreenState
                             '* Please type mnemonic (12 words) in your note for recover.',
                         labelText: 'Recover words',
                         onChanged: (String typedWords) {
-                          print('Recover');
-                          print(typedWords); // String
-                          print(mnemonic); // null
-                        },
-                      )
+//                          print('Recover');
+//                          print(typedWords); // String
+//                          print(mnemonic); // null
+                        })
                     : MnemonicVerificationTextField(
                         helperText:
                             '* Please type 1, 5, 9 words in your note for verification.',
@@ -49,34 +48,33 @@ class _VerificationOrRecoverScreenState
                           final listTypeWords = typedWords.split(' ');
 
                           setState(() {
-                            _wordsIsEmpty = typedWords.length == 0;
+                            _isShowWords = typedWords.length != 0;
+
                             /// check available and update in list
                             _listWords =
                                 listTypeWords.length > _verificationKeys.length
                                     ? _listWords
                                     : listTypeWords;
-
-                            // _listWords[0] == listMnemonicWords[0] && _listWords[1] == listMnemonicWords[4] && _listWords[2] == listMnemonicWords[8]
                           });
-                        },
-                      ),
+                        }),
                 SizedBox(height: 12.0),
-                _wordsIsEmpty
-                    ? SizedBox.shrink()
-                    : Wrap(
-                        spacing: 8.0,
-                        children: _listWords.asMap().entries.map((entry) {
-                          return mnemonic == null
-                              ? CustomChip(
-                                  index: (entry.key + 1), title: entry.value)
+                Wrap(
+                    spacing: 8.0,
+                    children: _isShowWords
+                        ? _listWords.asMap().entries.map((entry) {
+                            return mnemonic == null
+                                ? CustomChip(
+                                    index: (entry.key + 1), title: entry.value)
 
-                              /// case insurance verification key list undefined error
-                              : entry.key < _verificationKeys.length
-                                  ? CustomChip(
-                                      index: (_verificationKeys[entry.key] + 1),
-                                      title: entry.value)
-                                  : SizedBox.shrink();
-                        }).toList()),
+                                /// case insurance verification key list undefined error
+                                : entry.key < _verificationKeys.length
+                                    ? CustomChip(
+                                        index:
+                                            (_verificationKeys[entry.key] + 1),
+                                        title: entry.value)
+                                    : SizedBox.shrink();
+                          }).toList()
+                        : []),
               ],
             ),
           ),
