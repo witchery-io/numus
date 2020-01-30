8705938e427d26806a41500d1561a314b3dde39d
Edo <edo.g@witchery.io>
2020-01-30 17:08:55 +0400

    bloc 13

--- lib/src/screens/wallet_screen.dart
+++ lib/src/screens/wallet_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental/core/core.dart';
+import 'package:flutter_fundamental/src/widgets/widgets.dart';
 
 class WalletScreen extends StatelessWidget {
   final mnemonic;
@@ -7,6 +9,20 @@ class WalletScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Wallet: Mnemonic: $mnemonic'));
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(8.0),
+      child: ListBody(
+        children: <Widget>[
+          Text('Wallet', style: TextStyle(fontSize: 24.0)),
+          Text('Mnemonic: $mnemonic'),
+          CustomButton(
+              child: Text('Logout'),
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, Router.initial, (_) => false);
+              }),
+        ],
+      ),
+    );
   }
 }
