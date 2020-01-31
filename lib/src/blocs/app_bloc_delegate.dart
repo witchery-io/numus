7dda0ee7f8f7e4a1dc1f78e1e3bfee8796e04e93
Edo <edo.g@witchery.io>
2020-01-31 12:55:05 +0400

    bloc 31

--- lib/src/blocs/app_bloc_delegate.dart
+++ lib/src/blocs/app_bloc_delegate.dart
@@ -5,17 +5,24 @@ class AppBlocDelegate extends BlocDelegate {
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
     print('On Event: -->');
+    print('$bloc');
+    print('$event');
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stacktrace) {
     super.onError(bloc, error, stacktrace);
     print('On Error: -->');
+    print('$bloc');
+    print('$error');
+    print('$stacktrace');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
     print('On Transition: -->');
+    print('$bloc');
+    print('$transition');
   }
 }
