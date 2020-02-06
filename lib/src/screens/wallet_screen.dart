9d1e7be11d1e21ee504506a06b0efe30301a5d1b
Edo <edo.g@witchery.io>
2020-02-06 17:12:00 +0400

    new ref. 31

--- lib/src/screens/wallet_screen.dart
+++ lib/src/screens/wallet_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_fundamental/src/blocs/mnemonic/bloc.dart';
 import 'package:flutter_fundamental/src/blocs/tab/bloc.dart';
 import 'package:flutter_fundamental/src/models/app_tab.dart';
 import 'package:flutter_fundamental/src/widgets/widgets.dart';
+import 'package:multi_currency/multi_currency.dart';
 
 class WalletScreen extends StatefulWidget {
   final mnemonic;
@@ -15,6 +16,14 @@ class WalletScreen extends StatefulWidget {
 }
 
 class _WalletScreenState extends State<WalletScreen> {
+  MultiCurrency mc;
+
+  @override
+  void initState() {
+    mc = MultiCurrency(widget.mnemonic);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TabBloc, AppTab>(builder: (context, activeTab) {
@@ -25,7 +34,7 @@ class _WalletScreenState extends State<WalletScreen> {
           drawer: LeftMenu(
               onLogout: () =>
                   BlocProvider.of<MnemonicBloc>(context).add(RemoveMnemonic())),
-          body: activeTab == AppTab.general ? _WalletTab() : Games(),
+          body: activeTab == AppTab.general ? _WalletTab(mc: mc) : Games(),
           bottomNavigationBar: TabSelector(
               activeTab: activeTab,
               onTabSelected: (tab) =>
@@ -35,12 +44,49 @@ class _WalletScreenState extends State<WalletScreen> {
 }
 
 class _WalletTab extends StatelessWidget {
+  final MultiCurrency mc;
+
+  _WalletTab({@required this.mc});
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        padding: EdgeInsets.all(12.0),
-        child: ListBody(
-          children: <Widget>[Text('Content Wallet')],
-        ));
+    return Column(children: <Widget>[
+      Container(
+          margin: EdgeInsets.all(12.0),
+          child: Text('Currencies', style: TextStyle(fontSize: 24.0))),
+      Expanded(
+          child: ListView.separated(
+              itemCount: mc.getCurrencies.length,
+              separatorBuilder: (context, index) =>
+                  Divider(color: Colors.white),
+              itemBuilder: (context, index) {
+                final item = mc.getCurrencies[index];
+                return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: _Currency(item: item));
+              }))
+    ]);
+  }
+}
+
+class _Currency extends StatelessWidget {
+  final item;
+
+  _Currency({@required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('${item.name.toUpperCase()}', style: TextStyle(fontSize: 22.0)),
+          Text('Private Key: ${item.getPrivateKey()}'),
+          Text('Public Key: ${item.getPublicKey()}'),
+          FutureBuilder(
+              future: item.getAddress(),
+              builder: (context, snapshot) {
+                return Text('${snapshot.data}');
+              })
+        ]);
   }
 }
