8cca4f9ff1ec848e590e70482811366d22c288ea
Edo <edo.g@witchery.io>
2020-02-06 13:40:46 +0400

    new ref. 30

--- lib/src/widgets/left_menu.dart
+++ lib/src/widgets/left_menu.dart
@@ -10,10 +10,9 @@ class LeftMenu extends StatelessWidget {
     return Drawer(
       child: ListView(padding: EdgeInsets.zero, children: <Widget>[
         DrawerHeader(
-          decoration: BoxDecoration(color: Colors.deepOrange),
-          child: Text('Account',
-              style: TextStyle(color: Colors.white, fontSize: 24)),
-        ),
+            decoration: BoxDecoration(color: Colors.deepOrange),
+            child: Text('Account',
+                style: TextStyle(color: Colors.white, fontSize: 24))),
         ListTile(
             leading: Icon(Icons.delete_outline),
             title: Text('Log out'),
