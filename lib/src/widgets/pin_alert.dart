7f555b2c1fe59aa98358d9ed8696964d4930d1ce
Edo <edo.g@witchery.io>
2020-02-07 13:24:18 +0400

    new ref. 43

--- lib/src/widgets/pin_alert.dart
+++ lib/src/widgets/pin_alert.dart
@@ -3,50 +3,67 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:toast/toast.dart';
 
-class PinAlertDialog extends StatelessWidget {
-  final _pinController = TextEditingController();
+typedef OnConfirmCallback = Function(String pin);
+
+class PinAlertDialog extends StatefulWidget {
+  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final String title;
-  final Function _onConfirmed;
+  final OnConfirmCallback onConfirmed;
 
-  PinAlertDialog(this.title, this._onConfirmed);
+  PinAlertDialog({@required this.title, @required this.onConfirmed});
+
+  @override
+  _PinAlertDialogState createState() => _PinAlertDialogState();
+}
+
+class _PinAlertDialogState extends State<PinAlertDialog> {
+  String _pin;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-        title: Text('$title'),
+        title: Text('${widget.title}'),
         content: SingleChildScrollView(
-          child: ListBody(children: <Widget>[
-            Container(
-                padding: const EdgeInsets.all(4.0),
-                child: TextField(
-                    controller: _pinController,
-                    autofocus: true,
-                    maxLength: 6,
-                    decoration: InputDecoration(
-                      labelText: "Enter your PIN",
-                      helperStyle: TextStyle(color: Colors.deepOrange),
-                      helperText: "* Please set 6 symbole.",
-                      border: OutlineInputBorder(borderSide: BorderSide()),
-                    ),
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      WhitelistingTextInputFormatter.digitsOnly
-                    ])),
-          ]),
+          child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Form(
+                key: PinAlertDialog._formKey,
+                child: TextFormField(
+                  initialValue: '',
+                  autofocus: true,
+                  maxLength: 6,
+                  decoration: InputDecoration(
+                    labelText: "Enter your PIN",
+                    helperStyle: TextStyle(color: Colors.deepOrange),
+                    helperText: "* Please set 6 symbole.",
+                    border: OutlineInputBorder(borderSide: BorderSide()),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    WhitelistingTextInputFormatter.digitsOnly
+                  ],
+                  validator: (val) {
+                    if (val.trim().isEmpty) {
+                      return 'Please enter PIN.';
+                    } else if (val.trim().length != 6) {
+                      return 'Please enter 6 symbol';
+                    }
+
+                    return null;
+                  },
+                  onSaved: (value) => _pin = value,
+                ),
+              )),
         ),
         actions: <Widget>[
           FlatButton(
               child: Text('Confirmed'),
               onPressed: () async {
-                final strPin = _pinController.text;
-                if (strPin.length != 6)
-                  return Toast.show('Please set 6 symbol.', context,
-                      duration: 2, gravity: Toast.TOP);
-
-                _onConfirmed(convertToMd5(strPin));
-                _pinController.clear();
+                if (PinAlertDialog._formKey.currentState.validate()) {
+                  PinAlertDialog._formKey.currentState.save();
+                  widget.onConfirmed(convertToMd5(_pin));
+                }
               }),
         ]);
   }
