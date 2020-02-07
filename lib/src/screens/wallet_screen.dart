6cb4b31749b1a54fa170e166adacddcb71cad25f
Edo <edo.g@witchery.io>
2020-02-07 11:43:36 +0400

    new ref. 40

--- lib/src/screens/wallet_screen.dart
+++ lib/src/screens/wallet_screen.dart
@@ -6,27 +6,19 @@ import 'package:flutter_fundamental/src/blocs/wallet/bloc.dart';
 import 'package:flutter_fundamental/src/models/app_tab.dart';
 import 'package:flutter_fundamental/src/widgets/widgets.dart';
 
-class WalletScreen extends StatelessWidget {
+class WalletScreen extends StatefulWidget {
+  @override
+  _WalletScreenState createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<WalletBloc, WalletState>(builder: (context, state) {
       if (state is WalletLoading) {
         return LoadingIndicator();
       } else if (state is WalletLoaded) {
-        return BlocBuilder<TabBloc, AppTab>(builder: (context, activeTab) {
-          return Scaffold(
-              appBar: activeTab == AppTab.general
-                  ? AppBar(title: Text('Wallet'))
-                  : null,
-              drawer: LeftMenu(
-                  onLogout: () => BlocProvider.of<MnemonicBloc>(context)
-                      .add(RemoveMnemonic())),
-              body: activeTab == AppTab.general ? _WalletTab() : Games(),
-              bottomNavigationBar: TabSelector(
-                  activeTab: activeTab,
-                  onTabSelected: (tab) =>
-                      BlocProvider.of<TabBloc>(context).add(UpdateTab(tab))));
-        });
+        return _WalletScreen();
       } else {
         return null;
       }
@@ -34,9 +26,31 @@ class WalletScreen extends StatelessWidget {
   }
 }
 
+class _WalletScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TabBloc, AppTab>(builder: (context, activeTab) {
+      return Scaffold(
+          appBar: activeTab == AppTab.general
+              ? AppBar(title: Text('Wallet'))
+              : null,
+          drawer: LeftMenu(onLogout: () {
+            Navigator.pop(context);
+            BlocProvider.of<MnemonicBloc>(context).add(RemoveMnemonic());
+          }),
+          body: activeTab == AppTab.general ? _WalletTab() : Games(),
+          bottomNavigationBar: TabSelector(
+              activeTab: activeTab,
+              onTabSelected: (tab) =>
+                  BlocProvider.of<TabBloc>(context).add(UpdateTab(tab))));
+    });
+  }
+}
+
 class _WalletTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Wallet Content');
+    return Container(child: Text('Test'),);
   }
 }
+
