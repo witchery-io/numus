69d85bd25a13caf1a95fef563f2641513c7bc8e3
Edo <edo.g@witchery.io>
2020-02-17 15:55:57 +0400

    new ref. 128

--- lib/src/widgets/game_web_view.dart
+++ lib/src/widgets/game_web_view.dart
@@ -11,23 +11,25 @@ class GameWebView extends StatelessWidget {
 
   GameWebView({Key key, this.headers}) : super(key: key);
 
+  get webView => WebView(
+    initialUrl: url,
+    gestureNavigationEnabled: true,
+    javascriptMode: JavascriptMode.unrestricted,
+    onWebViewCreated: (WebViewController webViewController) async {
+      await webViewController.loadUrl(url, headers: headers);
+      _controller.complete(webViewController);
+    },
+    debuggingEnabled: true,
+    navigationDelegate: (NavigationRequest action) {
+      final isLink = action.url.contains(RegExp("^(http|https)://"), 0);
+      if (isLink) return NavigationDecision.navigate;
+
+      return Future.value(null);
+    },
+  );
+
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: url,
-      gestureNavigationEnabled: true,
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (WebViewController webViewController) async {
-        await webViewController.loadUrl(url, headers: headers);
-        _controller.complete(webViewController);
-      },
-      debuggingEnabled: true,
-      navigationDelegate: (NavigationRequest action) {
-        final isLink = action.url.contains(RegExp("^(http|https)://"), 0);
-        if (isLink) return NavigationDecision.navigate;
-
-        return Future.value(null);
-      },
-    );
+    return webView;
   }
 }
