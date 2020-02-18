537431e4d79cd71ba460ef9731422cd6dc550c11
Edo <edo.g@witchery.io>
2020-02-18 15:23:18 +0400

    new ref. 138

--- lib/src/widgets/game_web_view.dart
+++ lib/src/widgets/game_web_view.dart
@@ -1,37 +1,37 @@
 import 'dart:async';
 
-import 'package:bip21/bip21.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+typedef onDeepLink = Function(String deepLink);
 
 class GameWebView extends StatelessWidget {
   final url = 'https://531bd96c.ngrok.io/public';
   final Map<String, String> headers;
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
+  final onDeepLink deepLink;
 
-  GameWebView({Key key, this.headers}) : super(key: key);
+  GameWebView({Key key, this.deepLink, this.headers}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return WebView(
-      initialUrl: url,
-      gestureNavigationEnabled: true,
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (WebViewController webViewController) async {
-        await webViewController.loadUrl(url, headers: headers);
-        _controller.complete(webViewController);
-      },
-      debuggingEnabled: true,
-      navigationDelegate: (NavigationRequest action) {
-        final isLink = action.url.contains(RegExp("^(http|https)://"), 0);
-        if (isLink) return NavigationDecision.navigate;
+        initialUrl: url,
+        gestureNavigationEnabled: true,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) async {
+          await webViewController.loadUrl(url, headers: headers);
+          _controller.complete(webViewController);
+        },
+        debuggingEnabled: true,
+        navigationDelegate: (NavigationRequest action) {
+          final isLink = action.url.contains(RegExp("^(http|https)://"), 0);
+          if (isLink) return NavigationDecision.navigate;
 
-        final decoded = Bip21.decode(action.url);
-        print(decoded);
+          deepLink(action.url);
 
-        return Future.value(null);
-      },
-    );
+          return Future.value(null);
+        });
   }
 }
