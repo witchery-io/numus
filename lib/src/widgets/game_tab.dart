3d81d777d26f04c647479d8306c2433a0fcbb424
Edo <edo.g@witchery.io>
2020-02-18 16:58:34 +0400

    new ref. 145

--- lib/src/widgets/game_tab.dart
+++ lib/src/widgets/game_tab.dart
@@ -22,60 +22,7 @@ class GameTab extends StatelessWidget {
     return md5.convert(utf8.encode(val)).toString();
   }
 
-  const GameTab(
-      {Key key, this.showInvoiceDialog, this.currencies, this.isAuth = false})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    if (!isAuth)
-      return SafeArea(
-          child: GameWebView(
-              key: AppKeys.gameWebView,
-              onDeepLink: (link) {
-                Message.show(context, 'Please registration or login');
-              }));
-    else
-      return SafeArea(
-          child: FutureBuilder(
-              future: currencies.first,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) return Center(child: Text('Ups!'));
-
-                switch (snapshot.connectionState) {
-                  case ConnectionState.done:
-                    var jwtHeader;
-                    final Coin coin = snapshot.data;
-                    final md5Address = GameTab.convertMd5(coin.address);
-                    try {
-                      final token = _getToken(coin.privateKey, md5Address);
-                      jwtHeader = {
-                        HttpHeaders.authorizationHeader: 'Bearer ' + token
-                      };
-                    } catch (e) {
-                      Message.show(context, e.message);
-                    }
-                    return GameWebView(
-                        key: AppKeys.gameWebView,
-                        onDeepLink: (link) async {
-                          try {
-                            final decodeLink = Bip21.decode(link);
-                            showInvoiceDialog(
-                                decodeLink.address, decodeLink.amount);
-                          } catch (msg) {
-                            Message.show(context, msg);
-                          }
-                        },
-                        headers: jwtHeader);
-                  case ConnectionState.waiting:
-                    return LoadingIndicator(key: AppKeys.statsLoadingIndicator);
-                  default:
-                    return Container(key: AppKeys.emptyDetailsContainer);
-                }
-              }));
-  }
-
-  String _getToken(String privateKey, String jwtId) {
+  static String getSignToken(String privateKey, String jwtId) {
     try {
       return issueJwtHS256(
           JwtClaim(
@@ -89,5 +36,78 @@ class GameTab extends StatelessWidget {
     } catch (e) {
       throw Exception(e.message);
     }
+  }
+
+  const GameTab(
+      {Key key, this.showInvoiceDialog, this.currencies, this.isAuth = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isAuth)
+      return _GuestGameWidget();
+    else
+      return _GameWidget(currencies.first, showInvoiceDialog);
+  }
+}
+
+class _GuestGameWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: GameWebView(
+            key: AppKeys.gameWebView,
+            onDeepLink: (link) {
+              Message.show(context, 'Please registration or login');
+            }));
+  }
+}
+
+class _GameWidget extends StatelessWidget {
+  final Future<Coin> coin;
+  final OnShowInvoice showInvoiceDialog;
+
+  _GameWidget(this.coin, this.showInvoiceDialog);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: FutureBuilder(
+            future: coin,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) return Center(child: Text('Ups!'));
+
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  var jwtHeader;
+                  final Coin coin = snapshot.data;
+                  final md5Address = GameTab.convertMd5(coin.address);
+                  try {
+                    final token =
+                        GameTab.getSignToken(coin.privateKey, md5Address);
+                    jwtHeader = {
+                      HttpHeaders.authorizationHeader: 'Bearer ' + token
+                    };
+                  } catch (e) {
+                    Message.show(context, e.message);
+                  }
+                  return GameWebView(
+                      key: AppKeys.gameWebView,
+                      onDeepLink: (link) async {
+                        try {
+                          final decodeLink = Bip21.decode(link);
+                          showInvoiceDialog(
+                              decodeLink.address, decodeLink.amount);
+                        } catch (msg) {
+                          Message.show(context, msg);
+                        }
+                      },
+                      headers: jwtHeader);
+                case ConnectionState.waiting:
+                  return LoadingIndicator(key: AppKeys.statsLoadingIndicator);
+                default:
+                  return Container(key: AppKeys.emptyDetailsContainer);
+              }
+            }));
   }
 }
