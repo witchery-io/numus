bf7896ceac0261aa22e12dfc2cdd40e352493186
Edo <edo.g@witchery.io>
2020-03-03 12:30:07 +0400

    new global ref. 30

--- lib/src/repositories/crypto_repository.dart
+++ lib/src/repositories/crypto_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_fundamental/core/core.dart';
 import 'package:flutter_fundamental/src/models/balance.dart';
@@ -7,28 +9,42 @@ class CryptoRepository {
 
   const CryptoRepository({@required this.webClient});
 
-  Future<int> loadBalance(coin) async => _getBalance(coin);
+  Future loadBalance(coin) async {
+    try {
+      return await _getBalance(coin);
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
 
   Future<int> _getBalance(coin, {balance = 0, next = 20}) async {
     final Map addresses = coin.addresses(next: next);
 
     if (addresses.isEmpty) return Future.error('There aren\'t address');
 
-    final balanceStream = _streamBalance(next, coin.name, addresses);
-    final info = await _calcBalance(balanceStream, balance);
-
-    if (info['checkMore']) {
-      await _getBalance(coin, balance: info['balance'], next: next);
+    try {
+      final balanceStream = _streamBalance(next, coin.name, addresses);
+      final info = await _calcBalance(balanceStream, balance);
+      if (info['checkMore']) {
+        await _getBalance(coin, balance: info['balance'], next: next);
+      }
+      balance = info['balance'];
+    } catch (e) {
+      throw Exception(e);
     }
 
-    return info['balance'];
+    return balance;
   }
 
   Stream<Balance> _streamBalance(int next, String name, Map addresses) async* {
-    final to = addresses.length;
-    final from = to - next;
-    for (int i = from; i < to; i++) {
-      yield await webClient.getBalanceByAddress(name, addresses[i].address);
+    try {
+      final to = addresses.length;
+      final from = to - next;
+      for (int i = from; i < to; i++) {
+        yield await webClient.getBalanceByAddress(name, addresses[i].address);
+      }
+    } catch (e) {
+      throw Exception(e);
     }
   }
 
